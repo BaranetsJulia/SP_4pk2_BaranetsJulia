a433970c7df56308e03cf3f3098ba384bddc6773
Юлия Ивановна Баранец <baranets.yi1251@oksei.onmicrosoft.com>
2023-10-10 17:57:01 +0500

    pz_4

---
+++ pz_4/DataBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace pz_4
+{
+    public class DataBase //класс имитирует работу с БД
+    {
+        public void SaveData(string text) //метод, для вывода сообщения о сохранении данных
+        {
+            lock (this) //блокировка потока
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("SaveData: " + text);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ pz_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace pz_4
+{
+    class Program
+    {
+        public static DataBase db = new DataBase();
+
+        static void Main(string[] args)
+        {
+            Thread worker1 = new Thread(new ThreadStart(WorkerThreadMethod1));
+            Thread worker2 = new Thread(WorkerThreadMethod2);
+
+            worker1.Start();
+            worker2.Start();
+
+            Console.ReadLine();
+        }
+
+        static void WorkerThreadMethod1()
+        {
+            Console.WriteLine("Worker thread 1 started");
+            db.SaveData("Data from Worker thread 1");
+            Console.WriteLine("Worker thread 1 ended");
+        }
+
+        static void WorkerThreadMethod2()
+        {
+            Console.WriteLine("Worker thread 2 started");
+            db.SaveData("Data from Worker thread 2");
+            Console.WriteLine("Worker thread 2 ended");
+        }
+    }
+}
