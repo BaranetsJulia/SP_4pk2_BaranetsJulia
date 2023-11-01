be07d4295c27955aa0f0b8c25c23ff4ec4c2ead6
Юлия Ивановна Баранец <baranets.yi1251@oksei.onmicrosoft.com>
2023-11-01 19:58:40 +0500

    pz_14_15_16

--- pz_14/win.cpp
+++ pz_14/win.cpp
@@ -1,9 +1,21 @@
-#include <windows.h> //пз 14, 15
+#include <windows.h> //пз 14, 15, 16
+#include <string>
+#include "MathLib.h"
 
 HWND hwnd;
 
 // Прототип обработчика окна
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+HWND hButton1;
+HWND hButton2;
+HWND hButton3;
+HWND hButton4;
+HWND hButton5;
+HWND hButton6;
+HWND hInput1;
+HWND hInput2;
+HWND hOutput;
 
 // Главная функция
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -49,6 +61,30 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return 1;
     }
 
+    //Создание кнопок
+    hButton1 = CreateWindow(TEXT("BUTTON"), TEXT("+"), WS_VISIBLE | WS_CHILD,
+        10, 10, 50, 30, hwnd, NULL, hInstance, NULL);
+    hButton2 = CreateWindow(TEXT("BUTTON"), TEXT("-"), WS_VISIBLE | WS_CHILD,
+        60, 10, 50, 30, hwnd, NULL, hInstance, NULL);
+    hButton3 = CreateWindow(TEXT("BUTTON"), TEXT("*"), WS_VISIBLE | WS_CHILD,
+        110, 10, 50, 30, hwnd, NULL, hInstance, NULL);
+    hButton4 = CreateWindow(TEXT("BUTTON"), TEXT("/"), WS_VISIBLE | WS_CHILD,
+        160, 10, 50, 30, hwnd, NULL, hInstance, NULL);
+    hButton5 = CreateWindow(TEXT("BUTTON"), TEXT("pow"), WS_VISIBLE | WS_CHILD,
+        210, 10, 50, 30, hwnd, NULL, hInstance, NULL);
+    hButton6 = CreateWindow(TEXT("BUTTON"), TEXT("sqrt"), WS_VISIBLE | WS_CHILD,
+        260, 10, 50, 30, hwnd, NULL, hInstance, NULL);
+
+    //Поля ввода
+    hInput1 = CreateWindow(TEXT("EDIT"), TEXT(""), WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL,
+        10, 50, 200, 30, hwnd, NULL, hInstance, NULL);
+    hInput2 = CreateWindow(TEXT("EDIT"), TEXT(""), WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL,
+        10, 90, 200, 30, hwnd, NULL, hInstance, NULL);
+
+    //Поле вывода
+    hOutput = CreateWindow(TEXT("EDIT"), TEXT(""), WS_VISIBLE | WS_CHILD | WS_BORDER | ES_READONLY | ES_AUTOHSCROLL,
+        10, 130, 200, 30, hwnd, NULL, hInstance, NULL);
+
     // Отображение и обновление главного окна
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
@@ -72,13 +108,34 @@ bool a = true;
 // Обработчик окна
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    WCHAR buffer[] = L"Пять стихий";
+    WCHAR buffer[] = L"Выход из очереди";
     WCHAR buffer2[] = L"Заметка";
+    WCHAR buffer3[] = L"Окно перемещено";
+    WCHAR buffer4[] = L"Заметка";
+    WCHAR buffer5[] = L"Вычисление математических функций";
+    WCHAR buffer6[] = L"Заметка";
 
     switch (uMsg)
     {
     case WM_CLOSE:
         DestroyWindow(hwnd);
+        break;
+    case WM_RBUTTONDBLCLK:
+        MessageBox(
+            hwnd, // Родительское окно. Или NULL, если его нет.
+            buffer, // Выводимое сообщение.
+            buffer2, // Текст заголовка окна.
+            MB_ICONINFORMATION); // Настройки кнопок и значка окна сообщения.
+        return 1;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    case WM_MOVE:
+        MessageBox(
+            hwnd, // Родительское окно. Или NULL, если его нет.
+            buffer3, // Выводимое сообщение.
+            buffer4, // Текст заголовка окна.
+            MB_ICONINFORMATION); // Настройки кнопок и значка окна сообщения.
         break;
     case WM_ACTIVATE:
         if (a)
@@ -86,15 +143,115 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             a = false;
             MessageBox(
                 hwnd, // Родительское окно. Или NULL, если его нет.
-                buffer, // Выводимое сообщение.
-                buffer2, // Текст заголовка окна.
+                buffer5, // Выводимое сообщение.
+                buffer6, // Текст заголовка окна.
                 MB_ICONINFORMATION); // Настройки кнопок и значка окна сообщения.
         }
         break;
+    case WM_COMMAND:
+        //реакция на команды элементов управления
+        if (HIWORD(wParam) == BN_CLICKED && (HWND)lParam == hButton1)
+        {
+            //получение значений из полей ввода
+            TCHAR inputText1[256];
+            GetWindowText(hInput1, inputText1, sizeof(inputText1));
+            TCHAR inputText2[256];
+            GetWindowText(hInput2, inputText2, sizeof(inputText2));
+            //выполнение сложения
+            double result;
+            double value1 = std::stod(inputText1);
+            double value2 = std::stod(inputText2);
+            result = MathLibrary::Arithmetic::Add(value1, value2);
+            //запись результата в поле вывода
+            std::string outputText = std::to_string(result);
+            SetWindowText(hOutput, (LPCWSTR)outputText.c_str());
+        }
+        if (HIWORD(wParam) == BN_CLICKED && (HWND)lParam == hButton2)
+        {
+            //получение значений из полей ввода
+            TCHAR inputText1[256];
+            GetWindowText(hInput1, inputText1, sizeof(inputText1));
+            TCHAR inputText2[256];
+            GetWindowText(hInput2, inputText2, sizeof(inputText2));
+            //выполнение вычитания
+            double result;
+            double value1 = std::stod(inputText1);
+            double value2 = std::stod(inputText2);
+            result = MathLibrary::Arithmetic::Subtract(value1, value2);
+            //запись результата в поле вывода
+            std::string outputText = std::to_string(result);
+            SetWindowText(hOutput, (LPCWSTR)outputText.c_str());
+        }
+        if (HIWORD(wParam) == BN_CLICKED && (HWND)lParam == hButton3)
+        {
+            //получение значений из полей ввода
+            TCHAR inputText1[256];
+            GetWindowText(hInput1, inputText1, sizeof(inputText1));
+            TCHAR inputText2[256];
+            GetWindowText(hInput2, inputText2, sizeof(inputText2));
+            //выполнение умножения
+            double result;
+            double value1 = std::stod(inputText1);
+            double value2 = std::stod(inputText2);
+            result = MathLibrary::Arithmetic::Multiply(value1, value2);
+            //запись результата в поле вывода
+            std::string outputText = std::to_string(result);
+            SetWindowText(hOutput, (LPCWSTR)outputText.c_str());
+        }
+        if (HIWORD(wParam) == BN_CLICKED && (HWND)lParam == hButton4)
+        {
+            //получение значений из полей ввода
+            TCHAR inputText1[256];
+            GetWindowText(hInput1, inputText1, sizeof(inputText1));
+            TCHAR inputText2[256];
+            GetWindowText(hInput2, inputText2, sizeof(inputText2));
+            //выполнение деления
+            double result;
+            double value1 = std::stod(inputText1);
+            double value2 = std::stod(inputText2);
+            result = MathLibrary::Arithmetic::Divide(value1, value2);
+            //запись результата в поле вывода
+            std::string outputText = std::to_string(result);
+            SetWindowText(hOutput, (LPCWSTR)outputText.c_str());
+        }
+        if (HIWORD(wParam) == BN_CLICKED && (HWND)lParam == hButton5)
+        {
+            //получение значений из полей ввода
+            TCHAR inputText1[256];
+            GetWindowText(hInput1, inputText1, sizeof(inputText1));
+            TCHAR inputText2[256];
+            GetWindowText(hInput2, inputText2, sizeof(inputText2));
+            //выполнение возведения в степень
+            double result;
+            double value1 = std::stod(inputText1);
+            double value2 = std::stod(inputText2);
+            result = MathLibrary::Arithmetic::Exponentiation(value1, value2);
+            //запись результата в поле вывода
+            std::string outputText = std::to_string(result);
+            SetWindowText(hOutput, (LPCWSTR)outputText.c_str());
+        }
+        if (HIWORD(wParam) == BN_CLICKED && (HWND)lParam == hButton6)
+        {
+            //получение значений из полей ввода
+            TCHAR inputText1[256];
+            GetWindowText(hInput1, inputText1, sizeof(inputText1));
+            TCHAR inputText2[256];
+            GetWindowText(hInput2, inputText2, sizeof(inputText2));
+            //выполнение извлечения корня
+            double result;
+            double value1 = std::stod(inputText1);
+            double value2 = std::stod(inputText2);
+            result = MathLibrary::Arithmetic::Root(value1, value2);
+            //запись результата в поле вывода
+            std::string outputText = std::to_string(result);
+            SetWindowText(hOutput, (LPCWSTR)outputText.c_str());
+        }
+        break;
+
     case WM_PAINT:
         hDC = BeginPaint(hwnd, &ps);
         GetClientRect(hwnd, &rect);
-        DrawText(hDC, TEXT("Стихии природы: энергия, вода, огонь, земля, воздух!"), -1, &rect, DT_SINGLELINE |
+        DrawText(hDC, TEXT("Математические функции"), -1, &rect, DT_SINGLELINE |
             DT_CENTER | DT_VCENTER);
         EndPaint(hwnd, &ps);
         return 0;
